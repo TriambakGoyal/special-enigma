42bbd66e4168ee4558be5a730acb5d6c36dfc66c
TriambakGoyal <triambak.goyal@science.christuniversity.in>
2021-01-07 03:13:50 +0530

    First page UI complete error input left

--- main.js
+++ main.js
@@ -9,11 +9,16 @@ let mainWindow;
 app.on('ready', function() {
     //Create new window
     mainWindow = new BrowserWindow({
-        width: 480,
-        height: 550,
+        width: 435,
+        height: 600,
+        minHeight: 511,
+        minWidth: 435,
+        maximizable: false,
+        resizable: true,
         webPreferences: {
             nodeIntegration: true
         }
+
     });
     mainWindow.setMenu(null);
     //Loading HTML file
